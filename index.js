6df4253b0bc57752c939dded20e5cdb0859eeee0
Branimir Rijavec <rijavecbranko@gmail.com>
2023-01-19 19:52:57 +0200

    complete exercise 3.2

--- index.js
+++ index.js
@@ -24,13 +24,19 @@ let persons = [
   },
 ];
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/info", (request, response) => {
+  response.send(`
+    <p>Phonebook has info for ${persons.length} people </p>
+    <p>${new Date()}</p>
+  `);
+});
 
 app.get("/api/persons", (request, response) => {
-  console.log("Requesting persons");
-  response.json(persons)
+  response.json(persons);
 });
 
 const PORT = 3001;
