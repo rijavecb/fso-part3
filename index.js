bf279a5ffa526fc1ca913c76788a795d6c70848b
Branimir Rijavec <rijavecbranko@gmail.com>
2023-01-21 13:27:47 +0200

    complete exercises 3.7 and 3.8

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
-app.use(morgan('tiny'))
 app.use(express.json());
+
+app.use(morgan("tiny"));
+
+morgan.token("person", function (request, response) {
+  return JSON.stringify(request.body);
+});
+
+app.post(
+  "/api/persons",
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :person"
+  )
+);
 
 let persons = [
   {
