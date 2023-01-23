29a5f1bc61afff537138502b4befa75682baf9af
Branimir Rijavec <rijavecbranko@gmail.com>
2023-01-23 12:15:42 +0200

    complete exercise 3.9

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use(morgan("tiny"));
 
 morgan.token("person", function (request, response) {
@@ -100,7 +101,7 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
