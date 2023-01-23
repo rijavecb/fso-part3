2eb8dabc1da1b0b31920e1f7fb24b4ae85eed08d
Branimir Rijavec <rijavecbranko@gmail.com>
2023-01-23 12:40:01 +0200

    complete exercise 3.11

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(morgan("tiny"));
+app.use(express.static('build'))
 
 morgan.token("person", function (request, response) {
   return JSON.stringify(request.body);
